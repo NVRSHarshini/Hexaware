f3239a2b1172c4c753710739cc1c7dc6b8449194
Harshini N V R S <95141207+NVRSHarsnini@users.noreply.github.com>
2023-02-15 07:35:27 +0530

    Add files via upload

---
+++ Constructor_Overloading/Constructor_Overloading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructor_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Harshini!");
+            string[] doc =new string[] { "A", "B", "c" };
+            string[] ndoc = new string[] { "A", "B" };
+            Insurance i1= new Insurance();
+            Insurance i2= new Insurance("Sam",2500,19,doc);
+            Console.WriteLine(i2.checkeligibility());
+            Console.WriteLine(i1.checkeligibility());
+            Console.WriteLine(i2.checkdocuments(ndoc));
+        }
+    }
+}
---
+++ Inheritance_assignment/Inheritance_assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inheritance_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(1,"Harshini","tpt",123456789);
+            Person stu = new Student(1, "Sam", "Tpt", 1234567, 3, 97.5f, "A+", 49000.0f);
+            Console.WriteLine(person.Getdetails());
+            Console.WriteLine(stu.Getdetails());
+
+
+            
+
+            Teachingstaff t = new Teachingstaff(5201, "James", "USA", 123464576398, "professor", 100000, "Phd", "Chemistry");
+            Console.WriteLine("Teaching Staff");
+            Console.WriteLine(t.Getdetails());
+
+            NonTeachingstaff nt = new NonTeachingstaff(4202, "anji", "sklm", 1023468570086, "lab technician", 6000,  "cse", 60);
+            Console.WriteLine("Non teaching staff");
+            Console.WriteLine(nt.Getdetails());
+        }
+    }
+}

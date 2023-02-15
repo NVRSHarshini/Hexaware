18afef8bee77c2668a3b5a2c5876b030391c784b
Harshini N V R S <95141207+NVRSHarsnini@users.noreply.github.com>
2023-02-15 07:26:50 +0530

    Add files via upload

---
+++ Class_Methods_Assignment/Class_Methods_Assignment/Instructors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Methods_Assignment
+{
+   
+    
+        internal class Instructors
+        {
+            public string Name { get; set; }
+            public float avgfeedback { get; set; }
+            public int experience { get; set; }
+            public string[] instructorskill { get; set; }
+
+            public Instructors(string name, float feedback, int exp, string[] instructorskill)
+            {
+                this.Name = name;
+                this.avgfeedback = feedback;
+                this.experience = exp;
+                this.instructorskill = instructorskill;
+
+            }
+            public bool CheckSkill(string technology)
+            {
+
+            return validateligibility() && instructorskill.Contains(technology);
+
+        }
+
+            public bool validateligibility()
+            {
+
+                if (experience > 3 && avgfeedback >= 4.5)
+                {
+                    return true;
+                }
+                else if (experience <= 3 && avgfeedback >= 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Experience: " + experience);
+                    Console.WriteLine("Avg Feedback: " + avgfeedback);
+                    return false;
+                }
+            }
+
+        }
+
+    
+}
---
+++ Class_Methods_Assignment/Class_Methods_Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Class_Methods_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Harshini!");
+            string[] skills = new string[] { "C", "Java", "SQL" };
+            Instructors instructor = new Instructors("Harshini", 4.6f, 4, skills);
+            Console.WriteLine("Is instructor eligible: " + instructor.validateligibility());
+            Console.WriteLine("Does instructor have skill in python: " + instructor.CheckSkill("python"));
+        }
+    }
+}
